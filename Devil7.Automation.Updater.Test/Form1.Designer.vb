031201419bd0d1aab44a5d9d1f746f9116151ee8
Devil7DK <dineshthangavel47@gmail.com>
2018-08-17 01:20:22 +0530

    Initial Test Implementation

--- Devil7.Automation.Updater.Test/Form1.Designer.vb
+++ Devil7.Automation.Updater.Test/Form1.Designer.vb
@@ -22,10 +22,87 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.btn_GetAllReleases = New System.Windows.Forms.Button()
+        Me.gv_Releases = New System.Windows.Forms.DataGridView()
+        Me.StatusStrip = New System.Windows.Forms.StatusStrip()
+        Me.lbl_Status = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.prog_Status = New System.Windows.Forms.ToolStripProgressBar()
+        Me.UpdaterEx1 = New Devil7.Automation.Updater.UpdaterEx()
+        CType(Me.gv_Releases, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.StatusStrip.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btn_GetAllReleases
+        '
+        Me.btn_GetAllReleases.Location = New System.Drawing.Point(12, 342)
+        Me.btn_GetAllReleases.Name = "btn_GetAllReleases"
+        Me.btn_GetAllReleases.Size = New System.Drawing.Size(99, 23)
+        Me.btn_GetAllReleases.TabIndex = 0
+        Me.btn_GetAllReleases.Text = "Get All Releases"
+        Me.btn_GetAllReleases.UseVisualStyleBackColor = True
+        '
+        'gv_Releases
+        '
+        Me.gv_Releases.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.gv_Releases.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.gv_Releases.Location = New System.Drawing.Point(12, 9)
+        Me.gv_Releases.Name = "gv_Releases"
+        Me.gv_Releases.Size = New System.Drawing.Size(407, 327)
+        Me.gv_Releases.TabIndex = 3
+        '
+        'StatusStrip
+        '
+        Me.StatusStrip.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.lbl_Status, Me.prog_Status})
+        Me.StatusStrip.Location = New System.Drawing.Point(0, 371)
+        Me.StatusStrip.Name = "StatusStrip"
+        Me.StatusStrip.Size = New System.Drawing.Size(431, 22)
+        Me.StatusStrip.TabIndex = 4
+        Me.StatusStrip.Text = "StatusStrip1"
+        '
+        'lbl_Status
+        '
+        Me.lbl_Status.Name = "lbl_Status"
+        Me.lbl_Status.Size = New System.Drawing.Size(283, 17)
+        Me.lbl_Status.Spring = True
+        Me.lbl_Status.Text = "-"
+        Me.lbl_Status.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'prog_Status
+        '
+        Me.prog_Status.Name = "prog_Status"
+        Me.prog_Status.Size = New System.Drawing.Size(100, 16)
+        '
+        'UpdaterEx1
+        '
+        Me.UpdaterEx1.AssociatedAssembly = Nothing
+        Me.UpdaterEx1.Icon = Nothing
+        Me.UpdaterEx1.RepoName = "GSTR_2A-JSON_to_Excel_Converter"
+        Me.UpdaterEx1.Status = Nothing
+        Me.UpdaterEx1.UserName = "Devil7-Softwares"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(431, 393)
+        Me.Controls.Add(Me.StatusStrip)
+        Me.Controls.Add(Me.gv_Releases)
+        Me.Controls.Add(Me.btn_GetAllReleases)
+        Me.Name = "Form1"
+        Me.Text = "Devil7 Updater Library Test"
+        CType(Me.gv_Releases, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.StatusStrip.ResumeLayout(False)
+        Me.StatusStrip.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents btn_GetAllReleases As Button
+    Friend WithEvents gv_Releases As DataGridView
+    Friend WithEvents UpdaterEx1 As UpdaterEx
+    Friend WithEvents StatusStrip As StatusStrip
+    Friend WithEvents lbl_Status As ToolStripStatusLabel
+    Friend WithEvents prog_Status As ToolStripProgressBar
 End Class
--- Devil7.Automation.Updater.Test/Form1.vb
+++ Devil7.Automation.Updater.Test/Form1.vb
@@ -1,3 +1,18 @@
-﻿Public Class Form1
+﻿Imports Devil7.Automation.Updater
+Public Class Form1
+
+    Private Async Sub btn_GetAllReleases_Click(sender As Object, e As EventArgs) Handles btn_GetAllReleases.Click
+        btn_GetAllReleases.Enabled = False
+        gv_Releases.DataSource = Await UpdaterEx1.GetReleases
+        btn_GetAllReleases.Enabled = True
+        prog_Status.Value = 0
+    End Sub
+
+    Private Sub UpdaterEx1_StatusChanged(Sender As Object, StatusText As String, Progress As Integer, UpdateProgress As Boolean) Handles UpdaterEx1.StatusChanged
+        lbl_Status.Text = StatusText
+        If UpdateProgress Then
+            prog_Status.Value = Progress
+        End If
+    End Sub
 
 End Class
